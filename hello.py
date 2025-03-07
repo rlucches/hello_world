68d9e0d2d75144512ca209b4ee6059b6e9368d97
Rob Lucchesi <61019356+rlucches@users.noreply.github.com>
2025-03-07 13:51:21 -0500

    Update hello.py
    Fixed indentation

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print ("This is my first generation repository on GIThub.")
 def print_message(name='GitHub'):
-print(f"Welcome to the {name} tutorial!")
+    print(f"Welcome to the {name} tutorial!")
 print_message()
